f00a35bb3add5b6ec8c9eb78416012544a05f7e4
Jie Yan <jieyan620@gmail.com>
2020-03-26 11:40:51 -0700

    add login component, add switch rooter on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import LoginPage from './components/LoginPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
 
 function App() {
   return (
     <>
-    <h1>Welcome to Ratelations</h1>
+      <Router>
+        <div>
+          <Link to="/">login page </Link>
+          <br></br>
+          <Link to="/homepage">home page</Link>
+          <Switch>
+            <Route exact path="/">
+              <LoginPage />
+            </Route>
+            <Route path="/homepage">
+              <h1>This is homepage</h1>
+              <h1>Welcome to Ratelations</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+   
+  
     </>
   );
 }
