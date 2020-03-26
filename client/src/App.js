699755f1b7627f0d912f9c048c85b377670b2ee0
Jie Yan <jieyan620@gmail.com>
2020-03-26 13:09:30 -0700

    add components of homepage and topnave, half way

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import LoginPage from './components/LoginPage'
+import HomePage from './components/HomePage'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom'
+
 
 
 
@@ -23,8 +25,9 @@ function App() {
               <LoginPage />
             </Route>
             <Route path="/homepage">
-              <h1>This is homepage</h1>
-              <h1>Welcome to Ratelations</h1>
+              {/* <h1>This is homepage</h1>
+              <h1>Welcome to Ratelations</h1> */}
+              <HomePage />
             </Route>
           </Switch>
         </div>
