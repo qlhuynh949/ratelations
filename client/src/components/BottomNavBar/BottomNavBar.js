88b5b49078b78fd1dd266059242f592de2c9cb6e
Quan Huynh <qlhuynh@gmail.com>
2020-03-26 23:47:46 -0700

    Add top and bottom nav bars

---
+++ client/src/components/BottomNavBar/BottomNavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './BottomNavBar.css'
+
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import HistoryIcon from '@material-ui/icons/History';
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+const BottomNavBar = (props) => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  return (
+
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction label="Add" icon={<AddCircleIcon />} />
+      <BottomNavigationAction label="Past" icon={<HistoryIcon />} />
+
+    </BottomNavigation>
+
+  )
+}
+
+export default BottomNavBar
