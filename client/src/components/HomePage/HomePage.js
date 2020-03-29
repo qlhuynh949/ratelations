78e56eb8ad5d3260f403b2c065d23055212e3a34
Jie Yan <jieyan620@gmail.com>
2020-03-29 14:28:41 -0700

    edit display components

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PostInput from '../PostInput'
 import PostDisplay from '../PostDisplay'
 import CommentInput from '../CommentInput'
+import CommentDisplay from '../CommentDisplay'
 import Container from '@material-ui/core/Container'
 
 
--- client/src/components/PostDisplay/PostDisplay.js
+++ client/src/components/PostDisplay/PostDisplay.js
@@ -2,33 +2,21 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemText from '@material-ui/core/ListItemText'
 import IconButton from '@material-ui/core/IconButton'
-import FormGroup from '@material-ui/core/FormGroup'
-import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Typography from '@material-ui/core/Typography'
-import FolderIcon from '@material-ui/icons/Folder'
 import DeleteIcon from '@material-ui/icons/Delete'
 import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
+import CommentDisplay from '../CommentDisplay'
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     maxWidth: 752,
-  },
-  demo: {
     backgroundColor: theme.palette.background.paper,
   },
   title: {
     margin: theme.spacing(4, 0, 2),
-  },
-  inline: {
-    display: 'inline',
   },
   bullet: {
     display: 'inline-block',
@@ -38,9 +26,12 @@ const useStyles = makeStyles(theme => ({
   title: {
     fontSize: 14,
   },
-  pos: {
-    marginBottom: 12,
+  card:{
+    display: 'block',
   },
+  cardcontent:{
+    float:'right'
+  }
 }))
 
 function generate(element) {
@@ -59,44 +50,46 @@ const PostDisplay = () => {
       <Typography variant="h6" component="h2">
         Daily Post
           </Typography>
-      <div className={classes.demo}>
+      <div>
         <List >
           {generate(
-            <ListItem>
-              <Card className={classes.root}>
+            <ListItem className={classes.card}>
+              <Card >
                 <CardContent>
                   <Typography className={classes.title} color="textSecondary" gutterBottom>
                     post date
                   </Typography>
                   <Typography variant="h5" component="h2">
-                   Good things{bull}
+                    Good things{bull}
                   </Typography>
                   <Typography variant="body2" component="p">
-                    well meaning and kindly.
+                    well meaning and kindly.gfgaddfgafgagaga
                     <br />
                     {'"a benevolent smile"'}
                   </Typography>
+                    <br />
                   <Typography variant="h5" component="h2">
                     Bad things{bull}
                   </Typography>
                   <Typography variant="body2" component="p">
-                    well meaning and kindly.
+                    well meaning and kindly.hetehetthjtrhehetethehe
                     <br />
                     {'"a benevolent smile"'}
                   </Typography>
                 </CardContent>
+                <CardContent className={classes.cardcontent}>
+                   <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                   </IconButton>
+                </CardContent>
               </Card>
-              <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>,
+              <Card >
+                <CommentDisplay />
+              </Card>
+            </ListItem>
           )}
         </List>
       </div>
-
-
     </div>
 
   )
