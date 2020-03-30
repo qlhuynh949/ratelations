fedcb75893a49664f283d5e6c409a99fc4e5648e
Jie Yan <jieyan620@gmail.com>
2020-03-30 13:27:51 -0700

    edit postDisplay

---
+++ client/src/components/postData/postData.js
@@ -0,0 +1,28 @@
+
+ const postData = [
+  {
+    good: 'good one',
+    bad: 'bad one',
+    date:'3/4/2020'
+     },
+   {
+
+     good: 'good two',
+     bad: 'bad two',
+     date: '3/5/2020'
+   },
+   {
+
+     good: 'good 3',
+     bad: 'bad 3',
+     date: '3/6/2020'
+   },
+   {
+
+     good: 'good 4',
+     bad: 'bad 4',
+     date: '3/7/2020'
+   }
+];
+
+export default postData
