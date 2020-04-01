2a620d2b4e3e58fbdf9f70742f7fb7bf50aa0b99
Jie Yan <jieyan620@gmail.com>
2020-04-01 10:39:14 -0700

    props problem fixed

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PostInput from '../PostInput'
 import PostDisplay from '../PostDisplay'
 import CommentInput from '../CommentInput'
 import Container from '@material-ui/core/Container'
-
+import Item from '../../utils/Item'
 
 const HomePage = () => {
+  const [itemState, setItemState] = useState({
+    items: [],
+    score: '',
+    goodtext: '',
+    badtext: '',
+    isActive: true,
+    Ralationship: '',
+  })
+  const handleInputChange = ({ target }) => {
+    setItemState({ ...itemState, [target.name]: target.value })
+    console.log(target.value)
+  }
+  const handleCreateItem = (event) => {
+    event.preventDefault()
+    console.log('ping')
+    Item.create({
+      score: itemState.score,
+      goodtext: itemState.goodtext,
+      badtext: itemState.badtext,
+      isActive: true
+    })
+      .then(({ data: item }) => {
+        let items = JSON.parse(JSON.stringify(itemState.items))
+        items.push(item)
+        setItemState({ ...itemState, items, score: '', goodtext: '', badtext: '' })
+      })
+  }
 
   return (
     <Container component="main" maxWidth="xs">
-      <PostInput />
+      <PostInput handleInputChange={handleInputChange}
+        handleCreateItem={handleCreateItem} />
       <PostDisplay />
       <CommentInput />
     </Container>
