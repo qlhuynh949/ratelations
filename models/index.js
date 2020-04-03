13608507318e2d5de4f4a1e5cc136572af60e1fd
Quan Huynh <qlhuynh@gmail.com>
2020-04-02 17:34:00 -0700

    file name change

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ const User = require('./User.js')
 const Chart = require('./Chart.js')
 const Relationship = require('./Relationship.js')
 const Friends = require('./Friends.js')
-const Comments = require('./Comments.js')
+const Comments = require('./Comment.js')
 const ForgotPassword = require('./ForgotPassword.js')
 
 module.exports = {
